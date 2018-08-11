684fbb86d221ac3b3479b0fe7c4403b16a86add2
Jeff Chasin <McCloudFace1@gmail.com>
2018-08-11 16:25:33 -0400

    successfully get data to VideoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,11 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+  console.log(props.videos);
+  return (
+    <div className="video-list">
+      {props.videos.map((video) => <div><h5><em>!!videoListEntry</em> view goes here</h5></div>)}
+    </div>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +16,18 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
+/*
+
+let GroceryList = (props) => (
+  <ul>
+   {
+     props.list.map((item) => {
+       return <GroceryListItem item={item}/>;
+       })}
+  </ul>
+);
+
+
+*/
